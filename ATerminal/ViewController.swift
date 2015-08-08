c44bcfbb73afd034824ef5e53f82e120459886f9
mars-rover <mars-rover@sina.com>
2015-08-08 14:32:47 +0800

    Initial Commit

---
+++ ATerminal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATerminal
+//
+//  Created by lijunge on 15/8/8.
+//  Copyright © 2015年 A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
